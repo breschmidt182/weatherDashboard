c47c505969fa70578d6dcbd48a5cae081cfad255
Bre <breanna.schmidt12@gmail.com>
2022-05-15 11:44:28 -0400

    add coords function

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var search = $("#search");
+var searchBar = $("#search-bar");
+var searchBtn = $("#search-btn");
+var cords
+var city
+function getCords() {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=d83fb296c29f5fbb001985a71c347dac")
+        .then(response => response.json())
+        .then(data => {
+            cords = data.coord
+            getWeatherData(cords)
+        })
+        console.log("test")
+}
+getCords()
+
+function getWeatherData(coordinates) {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+coordinates.lat+"&lon="+coordinates.lon+"&appid=d83fb296c29f5fbb001985a71c347dac")
+    .then(response => response.json())
+    .then(data => {console.log(data)
+        console.log(data.daily)
+        console.log(data.daily[0])
+        console.log(data.daily[0].weather)
+        console.log(data.daily[0].weather[0])
+        console.log(data.daily[0].weather[0].main)
+        console.log(data.current.temp)
+    })
+}
