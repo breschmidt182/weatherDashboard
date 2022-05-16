b72015bc16ae2a96b5b8849b52c0ee1756257e55
Bre <breanna.schmidt12@gmail.com>
2022-05-15 21:41:42 -0400

    add search function and some css

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@ var search = $("#search");
 var searchBar = $("#search-bar");
 var searchBtn = $("#search-btn");
 var cords
-var city
+var city = $("#city")
+var results = $("#results")
+
 function getCords() {
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=d83fb296c29f5fbb001985a71c347dac")
         .then(response => response.json())
@@ -12,17 +14,30 @@ function getCords() {
         })
         console.log("test")
 }
-getCords()
 
 function getWeatherData(coordinates) {
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+coordinates.lat+"&lon="+coordinates.lon+"&appid=d83fb296c29f5fbb001985a71c347dac")
     .then(response => response.json())
     .then(data => {console.log(data)
-        console.log(data.daily)
-        console.log(data.daily[0])
-        console.log(data.daily[0].weather)
-        console.log(data.daily[0].weather[0])
         console.log(data.daily[0].weather[0].main)
         console.log(data.current.temp)
     })
 }
+
+function search () {
+    getCords();
+    getCords.append;
+    getWeatherData.append;
+}
+
+searchBtn.addEventListener("click", search);
+
+
+
+
+
+
+
+
+
+
